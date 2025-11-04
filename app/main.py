7431b350e0b93d34e99a9ebda3a9b538e9086321
IF-APP <careem029@gmail.com>
2025-11-04 12:42:49 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -182,7 +182,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://riderdashboard.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
